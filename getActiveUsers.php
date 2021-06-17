6200d905c24e5ca1b55f5a3f8e54caba697cb54a
Tiffany <saxorynaffit@gmail.com>
2021-06-17 08:41:35 +0800

    added img to active users

--- getActiveUsers.php
+++ getActiveUsers.php
@@ -6,15 +6,14 @@ $xml->load("users.xml");
 
 $userList = $xml->getElementsByTagName("user");
 
-$activeUsers = "Active Users:</br>";
+$activeUsers = "<p id='aUsers'>Active Users:</br></p>";
 foreach($userList as $user){
     $status = $user->getElementsByTagName("status")[0]->nodeValue;
     $usn = $user->getAttribute("username");
     $img = $user->getElementsByTagName("profilePic")[0]->nodeValue;
     if($status == "online"){
         if($_SESSION["activeUsers"] != $usn){
-            $activeUsers .= "<p class='active-users' onclick='printUSN(\"$usn\")'>$usn</p>";
-            // $activeUsers .= "<img class='user-img' src='$img'width='5px'/><p>$usn</p>";
+            $activeUsers .= "<img class='user-img' src='$img'width='5px'/><p class='active-users' onclick='printUSN(\"$usn\")'>$usn</p>";
         }
     }
 }
